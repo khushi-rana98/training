ef840276b889b8178bc252e952b485f80edae491
Khushi98 <khushi.rana7689@gmail.com>
2026-06-22 05:03:38 +0000

    prime check

--- day1.cpp
+++ day1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
+// question 1: prime check
+bool isPrime(int num){
+    if(num<2) return false;
+    for(int i=2;i*i<=num;i++){
+        if(num%i==0) return false;
+    }
+    return true;
+}
 int main(){
     cout<<"hey";
+    // prime check
+    int n;
+    cin>>n;
+    if(isPrime(n))
+    cout<<n<<" is a prime number";
     return 0;
 }
