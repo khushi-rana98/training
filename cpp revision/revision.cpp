1fcd1ba21ff9b0920f65645ced5c026ad2c61f65
Khushi98 <khushi.rana7689@gmail.com>
2026-06-22 05:54:34 +0000

    switch case

--- cpp revision/revision.cpp
+++ cpp revision/revision.cpp
@@ -14,5 +14,19 @@ int main(){
     cout<<"Exam will conduct";
     else
     cout<<"no surity";
+    char grade='A';
+    switch(grade){
+        case 'A':
+        cout<<"Excellent";
+        break;
+        case 'B':
+        cout<<"okay";
+        break;
+        case 'C':
+        cout<<"Avg";
+        break;
+        default:
+        cout<<"Invalid";
+    }
     return 0;
 }
--- day1.cpp
+++ day1.cpp
@@ -8,6 +8,19 @@ bool isPrime(int num){
     }
     return true;
 }
+int countFactors(int num){
+    int count=0;
+    for(int i=1;i*i<=num;i++){
+       if(num%i==0){
+        if(i==num/i)
+        count+=1;
+        else
+        count+=2;
+       }
+
+    }
+    return count;
+}
 int main(){
     cout<<"hey";
     // prime check
@@ -15,5 +28,13 @@ int main(){
     cin>>n;
     if(isPrime(n))
     cout<<n<<" is a prime number";
+    cout<<endl<<"Enter the number to count the factors of: ";
+    int num;
+    cin>>num;
+    if(countFactors(num))
+    cout<<"number of factors: "<<countFactors<<endl;
+    else
+    cout<<"NO factors";
+
     return 0;
 }
