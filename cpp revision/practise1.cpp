7d69576831aa97f38cc4acd2dfba4a4cc9ec1bd4
Khushi98 <khushi.rana7689@gmail.com>
2026-06-23 05:44:22 +0000

    vector usage revision..

--- cpp revision/practise1.cpp
+++ cpp revision/practise1.cpp
@@ -1,43 +1,67 @@
 #include <iostream>
 using namespace std;
-void swap(int &a, int &b){
-    int temp=a;
-    a=b;
-    b=temp;
-}
-void sortColors(int arr[], int n){
-    int low=0;
-    int high=n-1;
-    int mid=0;
-    while(mid<=high){
-        if(arr[mid]==2){
-            swap(arr[mid],arr[high]);
-            high--;
-        }
-        else if(arr[mid]==0){
-            swap(arr[low],arr[mid]);
-            low++;
-            mid++;
-        }
-        else{
-            mid++;
-        }
-    }
-}
+#include <vector>
 int main(){
-    
-    int n;
-    cout<<"Enter the number: ";
-    cin>>n;
-    int arr[n];
-    cout<<"Enter elmnts: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    vector<int>a;
+    a.push_back(5);
+    a.push_back(2);
+    a.push_back(5);
+    a.push_back(2);
+    a.push_back(5);
+  
+    cout<<"elmnts in vector are: ";
+    for(int i:a){
+        cout<<i<<" ";
     }
-    sortColors(arr,n);
-    cout<<"array after sorting: ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout<<endl;
+
+    cout<<"operation 2:"<<endl;
+    for(int i:a){
+        a.pop_back();
     }
-    return 0;
+   if(a.empty()){
+    cout<<"array is empty"<<endl;
+   }
+   
 }
+// void swap(int &a, int &b){
+//     int temp=a;
+//     a=b;
+//     b=temp;
+// }
+// void sortColors(int arr[], int n){
+//     int low=0;
+//     int high=n-1;
+//     int mid=0;
+//     while(mid<=high){
+//         if(arr[mid]==2){
+//             swap(arr[mid],arr[high]);
+//             high--;
+//         }
+//         else if(arr[mid]==0){
+//             swap(arr[low],arr[mid]);
+//             low++;
+//             mid++;
+//         }
+//         else{
+//             mid++;
+//         }
+//     }
+// }
+// int main(){
+    
+//     int n;
+//     cout<<"Enter the number: ";
+//     cin>>n;
+//     int arr[n];
+//     cout<<"Enter elmnts: ";
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     sortColors(arr,n);
+//     cout<<"array after sorting: ";
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+//     return 0;
+// }
