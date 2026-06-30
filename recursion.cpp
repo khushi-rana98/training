b09fabcd70e7b73385b102a0f46c56930cccb86f
Khushi98 <khushi.rana7689@gmail.com>
2026-06-30 04:08:27 +0000

    factorial  using recursion

--- recursion.cpp
+++ recursion.cpp
@@ -10,9 +10,15 @@ void print2(int n){
     cout<<n<<" ";
     print2(n+1);
 }
+int fact(int n){
+    if(n==0) return 1;
+    return n*fact(n-1);
+}
 int main(){
     printNumber(10);
     cout<<endl<<"Printing numbers 1 to 10: ";
     print2(1);
+    cout<<endl<<"Factorial of 5 using recursion: "<<fact(5);
+
     return 0;
 }
