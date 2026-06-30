d75e02f731865799a15a2da83bfccb40d7f7b21a
Khushi98 <khushi.rana7689@gmail.com>
2026-06-30 04:28:32 +0000

    fibonnaci

--- recursion.cpp
+++ recursion.cpp
@@ -14,11 +14,18 @@ int fact(int n){
     if(n==0) return 1;
     return n*fact(n-1);
 }
+int fib(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fib(n-1)+fib(n-2);
+
+}
 int main(){
     printNumber(10);
     cout<<endl<<"Printing numbers 1 to 10: ";
     print2(1);
     cout<<endl<<"Factorial of 5 using recursion: "<<fact(5);
-
+   
+    cout<<endl<<"10th fibonacci term : "<<fib(1);
     return 0;
 }
