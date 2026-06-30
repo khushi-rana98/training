278aec9bc14f88e92a39dc6eec06ef42b3ea5fef
Khushi98 <khushi.rana7689@gmail.com>
2026-06-30 05:37:04 +0000

    linear and binary search using recursion

--- recursion.cpp
+++ recursion.cpp
@@ -20,6 +20,19 @@ int fib(int n){
     return fib(n-1)+fib(n-2);
 
 }
+bool linearSearch(int arr[], int n, int key){
+    if(n==0) return false; 
+    if(arr[0]==key) return true;
+    return linearSearch(arr+1, n-1, key);
+}
+bool binarySearch(int a[], int s, int e, int key){
+    if(e<s) return false;
+    int mid=s+(e-s)/2;
+    if(a[mid]==key) return true;
+    else if(a[mid]>key) return binarySearch(a,0,mid-1,key);
+    else
+    return binarySearch(a,mid+1,e,key);
+}
 int main(){
     printNumber(10);
     cout<<endl<<"Printing numbers 1 to 10: ";
@@ -27,5 +40,20 @@ int main(){
     cout<<endl<<"Factorial of 5 using recursion: "<<fact(5);
    
     cout<<endl<<"10th fibonacci term : "<<fib(1);
+    int n=5;
+    int a[5]={10,20,50,15,23};
+    int target=15;
+    if(linearSearch(a,n,target))
+    cout<<"elmnt found!";
+    else
+    cout<<"elmnt not found!"<<endl;
+
+    int n2=6;
+    int b[6]={3, 2, 43, 21, 56, 43};
+    if(binarySearch(b,0,5,43))
+    cout<<"elmnt found";
+    else
+    cout<<"elmnt not found!";
+
     return 0;
 }
