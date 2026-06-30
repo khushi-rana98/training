4dc40d99cf52f8f2c823d9fd86033b9b26403299
Khushi98 <khushi.rana7689@gmail.com>
2026-06-30 06:19:17 +0000

    2d array . basics revision

---
+++ 2D.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int r;
+    cout<<"Enter the number of rows: ";
+    cin>>r;
+    int c;
+    cout<<"Enter the number of columns: ";
+    cin>>c;
+    cout<<"Enter elmnts of array: ";
+    int a[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<"elmnts of array: ";
+
+  for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+        cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+    cout<<endl<<"Elmnts of array at even columns: ";
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j+=2)
+        cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+}
