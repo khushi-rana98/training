805fde22a7cdc63fe135a6e20a9c1cb4b9910a01
Khushi98 <khushi.rana7689@gmail.com>
2026-06-30 03:57:16 +0000

    recursion

--- recursion.cpp
+++ recursion.cpp
@@ -5,7 +5,14 @@ void printNumber(int n){
     cout<<n<<" ";
     printNumber(n-1);
 }
+void print2(int n){
+    if(n==11) return;
+    cout<<n<<" ";
+    print2(n+1);
+}
 int main(){
     printNumber(10);
+    cout<<endl<<"Printing numbers 1 to 10: ";
+    print2(1);
     return 0;
 }
