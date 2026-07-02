8ec778cf51534a88df6d3f8dffa2542227027979
Khushi98 <khushi.rana7689@gmail.com>
2026-07-02 05:23:20 +0000

    strings

---
+++ stringpractise.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+bool isSubsequence(string s, string t){
+    int s1=0;
+    int t1=0;
+    while(s1<s.size() && t1<t.size()){
+        if(s[s1]==t[t1]){
+            s1++;
+            t1++;
+        }
+        else
+        t1++;
+    }
+    if(s1==s.size()) return true;
+    return false;
+
+}
+class Solution {
+  public:
+    string firstRepChar(string s) {
+        vector<bool>seen(256,false);
+        // string ans="";
+        for(char c:s){
+            if(seen[c]){
+                // ans+=c;
+                return {c};
+            }
+            seen[c]=true;
+        }
+        return "-1";
+    }
+};
+int main(){
+    // valid angram
+    //valid palindrom I
+    // valid palindrome ii
+    // is subsequnce
+    string s;
+    getline(cin,s);
+    string t;
+    getline(cin,t);
+    isSubsequence(s,t);
+    return 0;
+}
