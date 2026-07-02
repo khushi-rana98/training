11bf09a986b759b85301fedbc783f4be6632483d
Khushi98 <khushi.rana7689@gmail.com>
2026-07-02 05:58:44 +0000

    repeated character

--- stringpractise.cpp
+++ stringpractise.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 bool isSubsequence(string s, string t){
     int s1=0;
@@ -15,30 +17,48 @@ bool isSubsequence(string s, string t){
     return false;
 
 }
-class Solution {
-  public:
-    string firstRepChar(string s) {
-        vector<bool>seen(256,false);
-        // string ans="";
-        for(char c:s){
-            if(seen[c]){
-                // ans+=c;
-                return {c};
-            }
-            seen[c]=true;
+// class Solution {
+//   public:
+//     string firstRepChar(string s) {
+//         vector<bool>seen(256,false);
+//         // string ans="";
+//         for(char c:s){
+//             if(seen[c]){
+//                 // ans+=c;
+//                 return {c};
+//             }
+//             seen[c]=true;
+//         }
+//         return "-1";
+//     }
+// };
+string first(string s){
+    vector<bool>seen(256,false);
+    for(char c:s){
+        if(seen[c]){ //already seen: encountering for the 2nd time
+            return {c};
         }
-        return "-1";
+        seen[c]=true;
     }
-};
+    return "-1";
+}
 int main(){
     // valid angram
     //valid palindrom I
     // valid palindrome ii
     // is subsequnce
-    string s;
-    getline(cin,s);
-    string t;
-    getline(cin,t);
-    isSubsequence(s,t);
+    // string s;
+    // getline(cin,s);
+    // string t;
+    // getline(cin,t);
+    // isSubsequence(s,t);
+
+    // question 2: first repeated character hvin g2nd occeurence at lowest 
+    // index
+    string x;
+    cout<<"Enter the string: ";
+    getline(cin,x);
+    string ans=first(x);
+    cout<<"the repeated character hving its second occurence at lowest index is: "<<ans;
     return 0;
 }
