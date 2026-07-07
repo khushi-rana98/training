7482dd044b899ee9c6fa3580dbd831d4e340240c
Khushi98 <khushi.rana7689@gmail.com>
2026-07-07 05:25:44 +0000

    reversal of linked list

--- singlyLinkedList.cpp
+++ singlyLinkedList.cpp
@@ -17,8 +17,18 @@ Node* findMid(Node* root){
         fast=fast->next->next;
     }
     return slow;
-
+}
+Node* reversal(Node *head){
+    Node*curr=head;
+    Node* prev=NULL;
+    Node* fut=NULL;
+    while(curr){
+        fut=curr->next;
+        curr->next=prev;
+        
+    }
 }
 int main(){
     // slow and fast method: find mid
+    // reversal 
 }
