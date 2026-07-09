331ddca772bc5da1f07ff65e2f615e2d533d533e
Khushi98 <khushi.rana7689@gmail.com>
2026-07-09 04:04:26 +0000

    practise questions

---
+++ allLInkedLIst.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // partitioning linked list
+    // 0s , 1s and 2s
+//     Reverse Linked List
+// Find Middle Node
+// Detect Loop/Cycle
+// Remove Nth Node from End
+// Merge Two Sorted Lists
+// Palindrome Linked List
+// Intersection of Two Lists
+// insert at 
+// odd even in linked list
+// reverse linked list in k groups
+// Reverse in K Groups (for stronger companies)
+// Sort Linked List
+// Delete Node / Insert Node
+}
