e59226b8d4130b63d989cae905c8923da8921241
Khushi98 <khushi.rana7689@gmail.com>
2026-07-12 13:22:45 +0000

    reverse string using stack

---
+++ Stacks/problems.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+void reverse(vector<int>&a){
+    stack<int> s;
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        s.push(a[i]);
+    }
+    int i=0;
+    while(!s.empty()){
+       a[i++]=s.top();
+        s.pop();
+    }
+    
+}
+int main(){
+    // reverse array
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    reverse(a);
+    cout<<"reversed array: ";
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
