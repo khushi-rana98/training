acc9c91fbb315cd74748fcee9d8568ca6cbc3ac7
Khushi98 <khushi.rana7689@gmail.com>
2026-07-17 04:15:11 +0000

    left view of binsry tree

--- Trees/practise.cpp
+++ Trees/practise.cpp
@@ -204,4 +204,22 @@ int height(Node* root) {
         if(root->left && !root->left->left && !root->left->right) return root->left->data;
         return sum+sumLeftNodes(root->left)+sumLeftNodes(root->right);
     }
+    // left view of binary treee--
+     vector<int> leftView(Node *root) {
+       if(!root) return {};
+       queue<Node*>q;
+       q.push(root);
+       vector<int>ans;
+       while(!q.empty()){
+           int size=q.size();
+           for(int i=1;i<=size;i++){
+               Node* temp=q.front(); q.pop();
+               if(i==1)
+               ans.push_back(temp->data);
+               if(temp->left) q.push(temp->left);
+               if(temp->right) q.push(temp->right);
+           }
+       }
+       return ans;
+    }
 }; 
