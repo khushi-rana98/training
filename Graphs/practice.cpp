7085067c46decca91a10ce6f9832a78a31da2b9b
KhushiRana <khushi.rana7689@gmail.com>
2026-07-23 15:44:05 +0530

    rotting oranges--leetcode

--- Graphs/practice.cpp
+++ Graphs/practice.cpp
@@ -183,6 +183,49 @@ void printGraph(){
         }
         return image;
     }
+// rotting oranges problem--leetcode--important interview question
+// whenever they ask min. time required, spread or anything like this--use BFS
+     int orangesRotting(vector<vector<int>>& grid) {
+        int rows=grid.size();
+        int cols=grid[0].size();
+        queue<pair<int,int>>q;
+        int fresh=0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2)
+                q.push(make_pair(i,j));
+                else if(grid[i][j]==1)
+                fresh++;
+            }
+        }
+        int time=0;
+        // up down left right
+        int dr[4]={-1,1,0,0};
+        int dy[4]={0,0,-1,1};
+        while(!q.empty() && fresh>0){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                auto curr=q.front();
+                q.pop();
+                int r=curr.first;
+                int c=curr.second;
+                for(int k=0;k<4;k++){
+                    int nr=r+dr[k];
+                    int nc=c+dy[k];
+                if(nr>=0 && nr<rows && nc>=0 && 
+                    nc<cols &&
+                    grid[nr][nc]==1){
+                    grid[nr][nc]=2;
+                    fresh--;
+                    q.push(make_pair(nr,nc));
+                    }
+                }
+
+            }//size
+            time++;
+        }
+        return (fresh==0)?time:-1;
+    }
 // DETECT CYCLE IN AN UNDIRECTED GRAPH--
 // USING BFS AND DFS--
     bool bfs(int start, unordered_map<int,list<int>>&adj, vector<bool>&visited){
