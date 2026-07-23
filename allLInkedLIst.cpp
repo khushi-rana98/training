49016479315e316e0588244ff0252aa5e0ba1141
KhushiRana <khushi.rana7689@gmail.com>
2026-07-23 22:30:04 +0530

    reverse linked list---

--- allLInkedLIst.cpp
+++ allLInkedLIst.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 using namespace std;
-int main(){
-//    Partition List
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+class LinkedList{
+    public:
+    Node* head;
+    LinkedList(){
+        head=NULL;
+    }
+    // reverse Linked list:
+   Node* reverseList(Node* curr){
+        Node* prev=NULL;
+        while(curr){
+            Node* fut=curr->next; 
+            curr->next=prev;
+            prev=curr;
+            curr=fut;
+        }
+        return prev;
+    }
+    // reverse linked list using recursion:
+    Node* reverseList(Node* curr, Node*prev){
+        if(!curr) return prev;
+        Node* fut=curr->next;
+        curr->next=prev;
+        return reverseList(fut,curr);
+    }
+    void reverseRecursion(Node* head){
+        if(!head) return;
+        Node* prev=nullptr;
+        head=reverseList(head,prev);
+    }
+    //    Partition List
 // Odd Even Linked List
 // Sort List
-//     Reverse Linked List
+// Reverse Linked List
 // Find Middle Node
 // Detect Loop/Cycle
 // Remove Nth Node from End
@@ -17,4 +54,4 @@ int main(){
 // Reverse in K Groups (for stronger companies)
 // Sort Linked List
 // Delete Node / Insert Node
-}
+};
