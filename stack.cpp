3dc3a4eca4025191396c6bce96b48461638db10e
KhushiRana <khushi.rana7689@gmail.com>
2026-08-05 14:24:30 +0530

    valid parenthesis--

--- stack.cpp
+++ stack.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <stack>
 using namespace std;
+
+bool isValidParenthesis(string s) {
+        stack<char>st;
+        for(char c:s){
+            if(c=='(' || c=='[' || c=='{') // when we get open brackets. push them into stack
+            st.push(c);
+            else{
+                if(st.empty()) return false; // case: }]]] handling this 
+                    char top=st.top();
+                    st.pop();
+                    if(c==')' && top!='(') 
+                    return false;
+                    if(c==']' && top!='[')
+                    return false;
+                    if(c=='}' && top!='{')
+                    return false;
+                
+            }
+
+        }
+        return st.empty(); // handling this [( case: if stack is not empty then return false
+    }
 int main(){
     stack<int>s;
     s.push(2);
